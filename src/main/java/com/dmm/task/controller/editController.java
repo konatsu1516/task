d7c5886d5ad6942f3fb95ba00bcb345a87dcc4e0
konatsu-miyatake <100133364+konatsu1516@users.noreply.github.com>
2022-03-22 00:24:26 +0900

    更新、削除実装

--- src/main/java/com/dmm/task/controller/editController.java
+++ src/main/java/com/dmm/task/controller/editController.java
@@ -1,14 +1,51 @@
 package com.dmm.task.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.dmm.task.data.entity.Tasks;
+import com.dmm.task.data.repository.TasksRepository;
+import com.dmm.task.form.TasksForm;
+import com.dmm.task.service.AccountUserDetails;
 
 @Controller
 public class editController {
+	
+	@Autowired //@Autowiredアノテーションを付けると、Spring Bootが自動でインスタンスをInjectします。
+	  private TasksRepository taskRepository;
+	
 	@GetMapping("/main/edit/{id}")
-    public String edit(@PathVariable("id") Integer id) {
+    public String edit(@PathVariable("id") Integer id,Model model) {
+		//idを使ってデータを取る
+		//取ってきたタスクの内容を渡す
+		Tasks myTask = taskRepository.findById(id).get();
+		model.addAttribute("task", myTask);
         return "edit";
     }
+	
+	@PostMapping("/main/edit/{id}")
+    public String mainCreate(Model model,@PathVariable("id") Integer id,TasksForm tasksForm, @AuthenticationPrincipal AccountUserDetails user) {
+		Tasks myTask = taskRepository.findById(id).get();
+		 myTask.setName(user.getName());
+	     myTask.setTitle(tasksForm.getTitle());
+	     myTask.setDate(tasksForm.getDate());
+	     myTask.setText(tasksForm.getText());
+	     myTask.setDone(tasksForm.isDone());
+		taskRepository.save(myTask);
 
+      return "redirect:/main";
+
+    }
+
+	 @PostMapping("/main/delete/{id}")
+     public String deleteTask(@PathVariable("id") Integer id) {
+ 		// 指定したIDのユーザーを削除
+		 taskRepository.deleteById(id);
+ 		return "redirect:/main";
+ 	}
 }
--- src/main/java/com/dmm/task/controller/mainController.java
+++ src/main/java/com/dmm/task/controller/mainController.java
@@ -4,7 +4,9 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,15 +16,20 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.dmm.task.data.entity.Tasks;
+import com.dmm.task.data.entity.Users;
 import com.dmm.task.data.repository.TasksRepository;
+import com.dmm.task.data.repository.UsersRepository;
 
 @Controller
 public class mainController {
 	@Autowired //@Autowiredアノテーションを付けると、Spring Bootが自動でインスタンスをInjectします。
 	  private TasksRepository taskRepository;
 	
+	@Autowired
+	  private UsersRepository usersRepository;
+	
 	@GetMapping("/main")
-	 public String main(Model model) {
+	 public String main(Model model,String userName) {
 		
 		// 1. MultiMapインスタンスの作成
 		MultiValueMap<LocalDate, Tasks> tasks = new LinkedMultiValueMap<LocalDate, Tasks>();
@@ -58,17 +65,31 @@ public class mainController {
 		model.addAttribute("matrix", matrix);
 		model.addAttribute("week", week);
 		
-		
+		int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+		int thisMonth = Calendar.getInstance().get(Calendar.MONTH)+1;
+		model.addAttribute("thisYear", thisYear);
+		model.addAttribute("thisMonth", thisMonth);
 		
 		
 		List<Tasks> allTasks = taskRepository.findAll();
 		
-		for(int i=0;i<allTasks.size();i++) {
-			tasks.add(allTasks.get(i).getDate(), allTasks.get(i));	
+		//ユーザーごとのデータ絞り込み処理
+		// データベースからアカウント情報を取得する
+	    Users user = usersRepository.findById(userName).get();
+	    //allTasksリストをストリームにして新たなnotAdminTasksリストにする
+		List<Tasks> notAdminTasks = allTasks.stream()
+				//タスクのユーザーネームとアカウントのユーザーネームが同じものをフィルターかける
+				.filter(t -> t.getName() .equals (user.getName()))
+				//フィルターをかけたものを新たなリストとして生成
+				.collect(Collectors.toList());
+		
+		for(int i=0;i<notAdminTasks.size();i++) {
+			tasks.add(notAdminTasks.get(i).getDate(), notAdminTasks.get(i));	
 		}
-				
-		
-		
+			
+//		for(int i=0;i<allTasks.size();i++) {
+//			tasks.add(allTasks.get(i).getDate(), allTasks.get(i));	
+//		}
 		
 		model.addAttribute("tasks", tasks);
 		
